7a9166139385f0906a3a231f33ee8d445e554797
Christopher <chrispritchard.nz@gmail.com>
2019-03-16 08:08:23 +1300

    interim rework of diamond-square into paired loops

--- diamond-square/DiamondSquare.fs
+++ diamond-square/DiamondSquare.fs
@@ -30,43 +30,52 @@ let create dim seed =
         (if x < 0 then (dim-1) + x elif x >= dim then x - (dim - 1) else x),
         (if y < 0 then (dim-1) + y elif y >= dim then y - (dim - 1) else y)
 
-    let rec step x y size range =
-        // diamond step (set point in middle of square)
-        let corners = 
-            squarePoints x y size 
-            |> List.map arrayPoints
-            |> List.map (fun (x, y) -> (x, y), array.[x, y])
-        if corners |> List.tryFind (snd >> (=) 0.) <> None then
-            ()
-        let value = 
-            corners
-            |> List.sumBy snd
-            |> fun total -> min 1. ((total / 4.) + (random.NextDouble() * range))
-        let nsize = size/2
-        let mx, my = x + nsize, y + nsize
-        array.[mx, my] <- value
+    let rec step size range =
 
-        // square step (four new squares from diamond, above)
-        diamondPoints mx my nsize
-        |> List.iter (fun (cx, cy) ->
-            let points = 
-                diamondPoints cx cy nsize 
-                |> List.map arrayPoints
-                |> List.map (fun (x, y) -> (x, y), array.[x, y])
-            if points |> List.tryFind (snd >> (=) 0.) <> None then
-                ()
-            let value = 
-                points
-                |> List.sumBy snd
-                |> fun total -> min 1. ((total / 4.) + (random.NextDouble() * range))
-            array.[cx, cy] <- value)
+        let hsize = size/2
+
+        let mutable x, y = 0, 0
+        while x < dim - 1 do
+            while y < dim - 1 do
+                // diamond step (set point in middle of square)
+                let corners = 
+                    squarePoints x y size 
+                    |> List.map arrayPoints
+                    |> List.map (fun (x, y) -> (x, y), array.[x, y])
+                if corners |> List.tryFind (snd >> (=) 0.) <> None then
+                    ()
+                let value = 
+                    corners
+                    |> List.sumBy snd
+                    |> fun total -> min 1. ((total / 4.) + (random.NextDouble() * range))
+                
+                let mx, my = x + hsize, y + hsize
+                array.[mx, my] <- value
+                y <- y + size
+            x <- x + size
+        
+        let mutable mx, my = hsize, hsize
+        while mx < dim - 1 do
+            while my < dim - 1 do
+                // square step (four new squares from diamond, above)
+                for (cx, cy) in diamondPoints mx my hsize do
+                    let points = 
+                        diamondPoints cx cy hsize 
+                        |> List.map arrayPoints
+                        |> List.map (fun (x, y) -> (x, y), array.[x, y])
+                    if points |> List.tryFind (snd >> (=) 0.) <> None then
+                        ()
+                    let value = 
+                        points
+                        |> List.sumBy snd
+                        |> fun total -> min 1. ((total / 4.) + (random.NextDouble() * range))
+                    array.[cx, cy] <- value
+                my <- my + size
+            mx <- mx + size
        
         if size < 2 then ()
         else
-            step x y nsize (range/2.)
-            step (x + nsize) y nsize (range/2.)
-            step (x + nsize) (y + nsize) nsize (range/2.)
-            step x (y + nsize) nsize (range/2.)
+            step hsize (range/2.)
 
-    step 0 0 (dim-1) 0.5
+    step (dim-1) 0.5
     array
