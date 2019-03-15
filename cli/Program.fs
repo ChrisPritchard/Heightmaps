7d1b6f29d220c0c937c450ff26b1072e7c95830f
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-16 10:47:12 +1300

    cli will now regenerate the heightmap on the press of 'R'

--- cli/Program.fs
+++ cli/Program.fs
@@ -2,25 +2,26 @@
 open System
 open System.Runtime.InteropServices
 
-let showViaSDL heightmap =
-    let width, height = Array2D.length1 heightmap, Array2D.length2 heightmap
-    let mutable window, renderer = IntPtr.Zero, IntPtr.Zero
+let private asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
 
-    SDL_Init(SDL_INIT_VIDEO) |> ignore
-    let windowFlags = SDL_WindowFlags.SDL_WINDOW_SHOWN ||| SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS
-    SDL_CreateWindowAndRenderer(width, height, windowFlags, &window, &renderer) |> ignore
-
-    let asUint32 (r, g, b) = BitConverter.ToUInt32 (ReadOnlySpan [|b; g; r; 255uy|])
-
+let private updateTexture getArray width texture =
     let frameBuffer = 
-        heightmap 
+        getArray ()
         |> Seq.cast<float> 
         |> Seq.map (fun f -> let g = byte (int (f * 255.)) in asUint32 (g, g, g))
         |> Seq.toArray
 
-    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height)
     let bufferPtr = IntPtr ((Marshal.UnsafeAddrOfPinnedArrayElement (frameBuffer, 0)).ToPointer ())
     SDL_UpdateTexture(texture, IntPtr.Zero, bufferPtr, width * 4) |> ignore
+
+let private showViaSDL dim getArray =
+    let mutable window, renderer = IntPtr.Zero, IntPtr.Zero
+    SDL_Init(SDL_INIT_VIDEO) |> ignore
+    let windowFlags = SDL_WindowFlags.SDL_WINDOW_SHOWN ||| SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS
+    SDL_CreateWindowAndRenderer(dim, dim, windowFlags, &window, &renderer) |> ignore
+
+    let texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, dim, dim)
+    updateTexture getArray dim texture
 
     let mutable keyEvent = Unchecked.defaultof<SDL_KeyboardEvent>
 
@@ -29,7 +30,13 @@ let showViaSDL heightmap =
         SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero) |> ignore
         SDL_RenderPresent(renderer) |> ignore
 
-        if SDL_PollEvent(&keyEvent) <> 0 && keyEvent.``type`` = SDL_KEYDOWN && keyEvent.keysym.sym = SDLK_ESCAPE then ()
+        if SDL_PollEvent(&keyEvent) <> 0 && keyEvent.``type`` = SDL_KEYDOWN then
+            if keyEvent.keysym.sym = SDLK_ESCAPE then ()
+            elif keyEvent.keysym.sym = uint32 'r' then
+                updateTexture getArray dim texture
+                drawLoop ()
+            else
+                drawLoop ()
         else drawLoop ()
     
     drawLoop ()
@@ -40,7 +47,7 @@ let showViaSDL heightmap =
 
 [<EntryPoint>]
 let main _ =
-    let array = DiamondSquare.create 512 None
+    //let array = DiamondSquare.create 512 None
     //Bitmaps.PPM.grayscale "test.ppm" array
-    showViaSDL array
+    showViaSDL 513 (fun () -> DiamondSquare.create 512 None)
     0
