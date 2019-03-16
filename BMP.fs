bee34e1ef55ebca21217a0309152bd6a61fc63dd
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-16 17:25:45 +1300

    tried converting to 23bit, no success

--- BMP.fs
+++ BMP.fs
@@ -10,28 +10,20 @@ let grayscale fileName array =
     use out = File.OpenWrite fileName
 
     let width, height = Array2D.length1 array, Array2D.length2 array
-    let byteSize = width * height * 4
+    let padding = width % 4
+    let byteSize = ((width * 3) + padding) * height
 
     let dib = [
             yield! BitConverter.GetBytes (uint32 width)
             yield! BitConverter.GetBytes (uint32 height)
             yield! BitConverter.GetBytes (uint16 1)     // planes (=1)
-            yield! BitConverter.GetBytes (uint16 32)    // bpp (32bit)
-            yield! BitConverter.GetBytes (uint32 3)     // BI_BITFIELDS, no pixel array compression used 
-            yield! BitConverter.GetBytes (uint32 32)    // Size of the raw bitmap data (including padding) 
+            yield! BitConverter.GetBytes (uint16 24)    // bpp (32bit)
+            yield! BitConverter.GetBytes (uint32 0)     // BI_RGB, no pixel array compression used 
+            yield! BitConverter.GetBytes (uint32 byteSize)    // Size of the raw bitmap data (including padding) 
             yield! BitConverter.GetBytes (int32 2835)  // 72dpi horizontal
             yield! BitConverter.GetBytes (int32 2835)  // 72dpi vertical
             yield! BitConverter.GetBytes (uint32 0)     // palette colours (0, not using a palette)
             yield! BitConverter.GetBytes (uint32 0)     // important colours (0=all)
-            yield! [0uy;0uy;255uy;0uy]                  // red bit mask
-            yield! [0uy;255uy;0uy;0uy]                  // green bitmask
-            yield! [255uy;0uy;0uy;0uy]                  // blue bitmask
-            yield! [0uy;0uy;0uy;255uy]                  // alpha bitmask
-            yield! "Win " |> Seq.rev |> Seq.map byte    // LCS_WINDOWS_COLOR_SPACE
-            yield! List.replicate 36 0uy                // unused
-            yield! BitConverter.GetBytes (uint32 0)     // unused
-            yield! BitConverter.GetBytes (uint32 0)     // unused
-            yield! BitConverter.GetBytes (uint32 0)     // unused
         ]
 
     let header = [
@@ -52,7 +44,8 @@ let grayscale fileName array =
             for x = 0 to width-1 do
                 let v = array.[x, y]
                 let gs = int (v * 255.) |> byte
-                yield! [gs;gs;gs;255uy]
+                yield! [gs;gs;gs]
+            yield! List.replicate padding 0uy
     ] |> Seq.iter out.WriteByte
 
     out.Close ()
