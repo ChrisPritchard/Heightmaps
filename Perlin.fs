51c97c91eda30de165fed63ea6ea70d76bb131be
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-03-17 14:30:16 +1300

    added comments on perlin module

--- Perlin.fs
+++ Perlin.fs
@@ -1,5 +1,7 @@
 ﻿/// The code for perlin noise is taken almost verbatim (mainly language translations) from here:
 /// https://longwelwind.net/2017/02/09/perlin-noise.html
+/// This PDF on Simplex noise also has a good specification of Perlin noise and how it works:
+/// http://staffwww.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
 module Perlin
 
 open System
@@ -19,8 +21,10 @@ let private perlinVectors width height period random =
 
     vectors
 
-let private fade n =
-    (6. * (n ** 5.)) - (15. * (n ** 4.)) + (10. * (n ** 3.))
+/// This fade function provides a non-linear 'fudge' to the relative value.
+/// The algorithm is direct from the Perlin specification: 6t^5 - 15t^4 + 10t^3
+let private fade t =
+    (6. * (t ** 5.)) - (15. * (t ** 4.)) + (10. * (t ** 3.))
 
 let private dot (x1, y1) x2 y2 =
     (x1 * x2) + (y1 * y2)
@@ -48,11 +52,13 @@ let private perlinNoise x y (vectors: (float * float) [,]) period =
     let bottomLerp = lerp bottomLeftContribution bottomRightContribution relX
     let finalLerp = lerp topLerp bottomLerp relY
 
-    finalLerp / ((sqrt 2.) / 2.)
+    finalLerp / ((sqrt 2.) / 2.) // produces a final value between -1 and 1
 
 let create width height seed =
     let random = match seed with Some n -> Random n | _ -> Random ()
-    let period = ceil (float width / 10.)
+    // this period is fixed here based on width, 
+    // but could be changed to control the noise granularity
+    let period = ceil (float width / 10.) 
 
     let array = Array2D.create width height 0.
     let vectors = perlinVectors width height period random
